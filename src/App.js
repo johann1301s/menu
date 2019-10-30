90757a31b67264efcc09b58821f7f2067c6d1526
johann1301h <johann1301h@icloud.com>
2019-10-30 13:35:22 +0100

    add frame as inspired by stripe.com

--- src/App.js
+++ src/App.js
@@ -2,15 +2,39 @@ import React from 'react';
 import styled from 'styled-components'
 
 const Menu = styled.div`
-  height: 300px;
-  width: 100px;
-  background-color: red;
-`
+  height: 100%;
+  width: 100%;
+`;
+
+const Container = styled.div`
+  position: absolute;
+  left: calc(50% - 0.5 * 225px);
+  top: calc(50% - 0.5 * 467px);
+  border-radius: 38px;
+  background-color: white;
+  box-shadow: rgb(255, 255, 255) 0px 4px 7px 1px inset, rgba(173, 186, 204, 0.247059) 0px -5px 20px 0px inset, rgba(0, 21, 64, 0.137255) 0px 2px 6px 0px, rgba(0, 21, 64, 0.0470588) 0px 10px 20px 0px;
+  height: 467px;
+  width: 225px;
+`;
+
+const Content = styled.div`
+  position: absolute;
+  top: 46px;
+  left: 7px;
+  height: 375px;
+  width: 211px;
+  overflow: hidden;
+  background-color: lightgray;
+`;
 
 function App() {
   return (
     <div className="App">
-      <Menu/>
+      <Container>
+        <Content>
+          <Menu/>
+        </Content>
+      </Container>
     </div>
   );
 }
