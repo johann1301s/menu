55e7bf59b8593cfa9e6aea18eb025df2bcb33b29
johann1301h <johann1301h@icloud.com>
2019-10-30 13:31:00 +0100

    create first styled component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import styled from 'styled-components'
+
+const Menu = styled.div`
+  height: 300px;
+  width: 100px;
+  background-color: red;
+`
 
 function App() {
   return (
     <div className="App">
-
+      <Menu/>
     </div>
   );
 }
